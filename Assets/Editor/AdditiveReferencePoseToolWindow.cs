03d54588be4ac9762562fa99da3783eff65b3748
tsgcpp <tsgcpp.public8x@gmail.com>
2020-08-10 18:03:47 +0900

    Add resources for the sample.

---
+++ Assets/Editor/AdditiveReferencePoseToolWindow.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AdditiveReferencePoseToolWindow : EditorWindow
+{
+    [SerializeField] private List<AnimationClip> targetClipList = new List<AnimationClip>(64);
+
+    [SerializeField] private AnimationClip referenceClip = null;
+
+    [SerializeField] private float poseTime = 0.0f;
+
+    private Editor editor;
+
+    [MenuItem("Additive Reference Pose Tool/Tool Window")]
+    public static void Init()
+    {
+        EditorWindow.GetWindow(typeof(AdditiveReferencePoseToolWindow), false);
+    }
+
+    void OnEnable()
+    {
+        editor = Editor.CreateEditor(this);
+    }
+    
+    void OnGUI()
+    {
+        editor.OnInspectorGUI();
+    }
+
+    private void SetAdditiveReferencePose()
+    {
+        foreach (var targetClip in targetClipList) {
+            AnimationUtility.SetAdditiveReferencePose(targetClip, referenceClip, poseTime);
+        }
+    }
+
+    private void ResetAdditiveReferencePose()
+    {
+        foreach (var targetClip in targetClipList) {
+            AnimationUtility.SetAdditiveReferencePose(targetClip, null, 0.0f);
+        }
+    }
+
+    [CustomEditor(typeof(AdditiveReferencePoseToolWindow), true)]
+    private class WindowEditor : Editor {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+
+            var window = target as AdditiveReferencePoseToolWindow;
+
+            // 各種ボタンの追加
+
+            if (GUILayout.Button("Set Additive Reference Pose")) {
+                window.SetAdditiveReferencePose();
+                AssetDatabase.SaveAssets();
+                Debug.Log("Completed: Set Additive Reference Pose");
+            }
+
+            if (GUILayout.Button("Reset Additive Reference Pose")) {
+                window.ResetAdditiveReferencePose();
+                AssetDatabase.SaveAssets();
+                Debug.Log("Completed: Reset Additive Reference Pose");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HandCloseController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCloseController : MonoBehaviour
+{
+    public Animator animator;
+
+    public string thumbParam = "CloseThumb_L";
+    private int _thumbId = -1;
+
+    public string indexParam = "CloseIndex_L";
+    private int _indexId = -1;
+
+    public string middleParam = "CloseMiddle_L";
+    private int _middleId = -1;
+
+    public string ringParam = "CloseRing_L";
+    private int _ringId = -1;
+
+    public string pinkyParam = "ClosePinky_L";
+    private int _pinkyId = -1;
+
+    private void OnEnable()
+    {
+        _thumbId = Animator.StringToHash(thumbParam);
+        _indexId = Animator.StringToHash(indexParam);
+        _middleId = Animator.StringToHash(middleParam);
+        _ringId = Animator.StringToHash(ringParam);
+        _pinkyId = Animator.StringToHash(pinkyParam);
+    }
+
+    public void CloseThumb(float value)
+    {
+        animator.SetFloat(_thumbId, value);
+    }
+
+    public void CloseIndex(float value)
+    {
+        animator.SetFloat(_indexId, value);
+    }
+
+    public void CloseMiddle(float value)
+    {
+        animator.SetFloat(_middleId, value);
+    }
+
+    public void CloseRing(float value)
+    {
+        animator.SetFloat(_ringId, value);
+    }
+
+    public void ClosePinky(float value)
+    {
+        animator.SetFloat(_pinkyId, value);
+    }
+}
